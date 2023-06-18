e8383f430ce87c7936bc435e4ff6470091ffd9c0
Christoph Schmatzler <christoph@medium.place>
2023-06-18 14:40:09 +0200

    wip Summary:
    Test Plan:

--- clusters/_template/data.tf
+++
@@ -1,9 +0,0 @@
-data "sops_file" "control_plane_config" {
-  source_file = "talos/controlplane.yaml.sops"
-  input_type  = "yaml"
-}
-
-data "sops_file" "worker_config" {
-  source_file = "talos/worker.yaml.sops"
-  input_type  = "yaml"
-}
