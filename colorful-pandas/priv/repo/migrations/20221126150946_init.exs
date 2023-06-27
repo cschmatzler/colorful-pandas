c4e73ff1eaf7c415d3286c5641cbb6cdb565cbdc
Christoph Schmatzler <christoph@medium.place>
2023-06-27 22:34:42 +0200

    Fix migration lint
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- colorful-pandas/priv/repo/migrations/20221126150946_init.exs
+++ colorful-pandas/priv/repo/migrations/20221126150946_init.exs
@@ -2,6 +2,7 @@ defmodule ColorfulPandas.Repo.Migrations.Init do
   use Ecto.Migration
 
   # excellent_migrations:safety-assured-for-this-file raw_sql_executed
+
   def change do
     execute "create extension if not exists citext", "drop extension citext"
   end
--- colorful-pandas/priv/repo/migrations/20230328170807_add_identities.exs
+++ colorful-pandas/priv/repo/migrations/20230328170807_add_identities.exs
@@ -1,6 +1,8 @@
 defmodule ColorfulPandas.Repo.Migrations.AddIdentities do
   use Ecto.Migration
 
+  # excellent_migrations:safety-assured-for-this-file raw_sql_executed
+
   def change do
     role_create_query = "CREATE TYPE identities_role AS ENUM ('user', 'admin')"
     role_drop_query = "DROP TYPE identities_role"
