b9e583c2f688904facde668cc6de052be3dd1298
Christoph Schmatzler <christoph@medium.place>
2023-06-28 21:27:25 +0200

    Add Terraform workspace for Grafana Cloud
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

---
+++ tooling/terraform-cloud/tooling-grafana-cloud.tf
@@ -0,0 +1,19 @@
+resource "tfe_workspace" "grafana_cloud" {
+  organization       = tfe_organization.colorful_pandas.name
+  project_id         = tfe_project.colorful_pandas.id
+  name               = "grafana_cloud"
+  auto_apply         = true
+  allow_destroy_plan = false
+  terraform_version  = "1.5.1"
+  working_directory  = "tooling/grafana-cloud"
+  vcs_repo {
+    identifier     = "cschmatzler/colorful-pandas"
+    branch         = "main"
+    oauth_token_id = tfe_oauth_client.github.oauth_token_id
+  }
+}
+
+resource "tfe_workspace_variable_set" "grafana_cloud_api_tokens" {
+  workspace_id    = tfe_workspace.grafana_cloud.id
+  variable_set_id = tfe_variable_set.api_tokens.id
+}
