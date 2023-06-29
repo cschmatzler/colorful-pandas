5b01c911ab3162e3dfbd3fbddce81a5ec2b98cef
Christoph Schmatzler <christoph@medium.place>
2023-06-29 22:55:23 +0200

    Trigger rebuild
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- colorful-pandas/lib/colorful_pandas/web/pages/landing.ex
+++ colorful-pandas/lib/colorful_pandas/web/pages/landing.ex
@@ -20,7 +20,7 @@ defmodule ColorfulPandas.Web.Pages.Landing do
             Stuff.
           </h1>
           <p class="mt-6 text-lg leading-8 text-moss/80">
-            ... and more.
+            ... and more. Some day.
           </p>
         </div>
       </div>
